e1f7651fcd0813dbf7e0f05dd1e0f41ed3211349
octopengj <octopengj@google.com>
2021-03-12 17:47:18 +0900

    api URL 주소 변경 수정

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "http://grab-market-server-inf.herokuapp.com"
+    ? "https://grab-market-server-inf.herokuapp.com"
     : "http://localhost:8080";
